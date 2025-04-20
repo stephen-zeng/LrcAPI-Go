01ef9a01d318119060ac7fe09e1082e83c9a38dd
Stephen Zeng <stephenzeng@goforit.top>
2025-04-21 01:33:42 +0800

    feat: 增加qq音乐的API

--- util/other.go
+++ util/other.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 )
 
@@ -17,4 +18,21 @@ func ErrorPrinter(err error) {
 			}
 		}
 	}
+}
+
+// 将请求伪装成浏览器
+func SetHeader(req *http.Request) {
+	req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("accept-language", "en-US,en;q=0.9")
+	req.Header.Set("cache-control", "max-age=0")
+	req.Header.Set("priority", "u=0, i")
+	req.Header.Set("sec-ch-ua", `"Chromium";v="135", "Not-A.Brand";v="8"`)
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+	req.Header.Set("sec-fetch-dest", "document")
+	req.Header.Set("sec-fetch-mode", "navigate")
+	req.Header.Set("sec-fetch-site", "none")
+	req.Header.Set("sec-fetch-user", "?1")
+	req.Header.Set("upgrade-insecure-requests", "1")
+	req.Header.Set("user-agent", UA)
 }
