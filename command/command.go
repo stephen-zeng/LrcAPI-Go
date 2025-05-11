7934b27398002670ceb53265b782f8071a1f18a9
Stephen Zeng <stephenzeng@goforit.top>
2025-05-11 19:41:54 +0800

    feat: 添加figlet

--- command/command.go
+++ command/command.go
@@ -1,8 +1,13 @@
 package command
 
-import "os"
+import (
+	"github.com/common-nighthawk/go-figure"
+	"os"
+)
 
 func Arg(args []string) {
+	figlet := figure.NewFigure("LrcAPI-Go", "", true)
+	figlet.Print()
 	for index, arg := range args {
 		if index == 0 {
 			continue
